65f5c6b407ef2d1c3c36dca7ce1242f10a92447b
JV-X <xjv1195275315@gmail.com>
2023-06-19 11:45:33 +0800

    添加数据库配置

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask
+from exts import db
+import config
 
 app = Flask(__name__)
+app.config.from_object(config)
 
+db.init_app(app)
 
 @app.route('/')
 def hello_world():  # put application's code here
---
+++ config.py
@@ -0,0 +1,10 @@
+DB_USERNAME = 'root'
+DB_PASSWORD = 'xjv123..'
+DB_HOST = '127.0.0.1'
+DB_PORT = '3306'
+DB_NAME = 'pythonbbs'
+
+DB_URI = 'mysql+pymysql://%s:%s@%s:%s/%s?charset=utf8mb4' % (DB_USERNAME, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME)
+
+SQLALCHEMY_DATABASE_URI = DB_URI
+SQLALCHEMY_TRACK_MODIFICATIONS = False
