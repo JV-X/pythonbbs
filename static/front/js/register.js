34957c9ba4f46f4b5817ec508b74094f6c916170
JV-X <xjv1195275315@gmail.com>
2023-06-21 18:11:50 +0800

    验证码按钮倒计时实现

--- static/front/js/register.js
+++ static/front/js/register.js
@@ -1,22 +1,41 @@
 var RegisterHandler = function () {
 
 }
-
-RegisterHandler.prototype.listenSendCaptchaEvent = function () {
-    $('#email-captcha-btn').on('click',function (event){
-        event.preventDefault()
-        var email = $('input[name="email"]').val()
-        if (!email) {
-            alert("请输入正确格式的邮箱")
-            return;
-        }
+var callback = function (event){
+    var $this = $(this)
+    event.preventDefault()
+    var email = $('input[name="email"]').val()
+    if (!email) {
+        alert("请输入正确格式的邮箱")
+        return;
+    }
     zlajax.get({
         url: "email/captcha?email=" + email,
         success: function (result) {
-            console.log(result)
+            if(result['code']==200){
+                $this.off("click")
+                $this.attr("disabled",'disabled');
+                var countDown = 6;
+                var interval = setInterval(function(){
+                    if(countDown > 0){
+                        $this.text(countDown)
+                    } else {
+                        $this.text('发送验证码')
+                        $this.attr('disabled', false)
+                        $this.on('click',callback)
+                        clearInterval(interval)
+                    }
+                    countDown--
+                },1000)
+
+            }
         }
     })
-    });
+}
+
+RegisterHandler.prototype.listenSendCaptchaEvent = function () {
+
+    $('#email-captcha-btn').on('click',callback);
 }
 
 RegisterHandler.prototype.run = function () {
